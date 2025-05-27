5fc41cb5c01b6f57172ef66270751fb169b662f4
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-27 16:16:58 +0200

    Creata la select

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import movies from "../../data/Movies";
 import MoviesList from "../movies/MoviesList";
 
-export default function Main(){
+export default function Main({movie}){
+    const [filter, setFilter] = useState("");
     return(
         <main>
             <div className="container">
-                <h1>My Movies</h1>
+                <h1>Movies</h1>
+                <select className="form-select form-select-lg mb-3" aria-label="Default select example">
+                    <option selected>Genere film</option>
+                    <option value="1">Azione</option>
+                    <option value="2">Fantascienza</option>
+                    <option value="3">Giallo</option>
+                    <option value="4">Horror</option>
+                    <option value="5">Romantico</option>
+                    <option value="6">Thriller</option>
+                </select>
                 <MoviesList movies={movies}/>
             </div>
         </main>
